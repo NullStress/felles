c72bbbd91be9bc97912822abd9820180da79fba0
Kristian Lund <kristian.lund@kantega.no>
2018-10-04 04:40:55 +0200

    formatting account number

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -3,11 +3,16 @@ import {hot} from "react-hot-loader";
 import "./Account.css";
 
 class Account extends Component{
+
+    static formatNumber(number) {
+        return number.substring(0,4) + "." + number.substring(4,6) + "." + number.substring(6,11)
+    }
+
     render(){
         const account = this.props.value;
         return(
             <div className="Account">
-                <span>{account.name}</span><span>{account.number}</span>
+                <span>{account.name}</span><span>{Account.formatNumber(account.number)}</span>
             </div>
         );
     }
