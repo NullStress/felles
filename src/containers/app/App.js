e08f8dc5a22f53257b36a1acafcce339b4288a5b
Kristian Lund <kristian.lund@kantega.no>
2018-10-04 03:59:48 +0200

    Object at top in api.json, setting accounts on state

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -3,9 +3,46 @@ import {hot} from "react-hot-loader";
 import Accounts from "../accounts/Accounts"
 import Person from "../../components/person/Person"
 import "./App.css";
+import axios from "axios";
+import * as Constants from "../../constants";
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            persons: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(Constants.BASE_PATH + "persons")
+            .then(response => {
+                // handle success
+                console.log("success");
+                this.setState({persons: response.data});
+            })
+            .catch(error => {
+                // handle error
+                console.log(error);
+            })
+    }
+
+    getAccounts(ssn) {
+        let accounts = [];
+        if(this.state.persons[0]) {
+            this.state.persons.forEach(person => {
+                if(person.ssn === (ssn)) {
+                    accounts = person.accounts;
+                }
+            })
+        }
+        return accounts;
+    }
+
     render() {
+        let accounts = this.getAccounts("10128512336");
+        console.log(accounts);
         return (
             <div className="App">
                 <Person/>
